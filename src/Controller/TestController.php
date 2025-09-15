a39669ac9e810abac7dc9efb3e17bc4708a22271
Maxym Linenko <linenko.maxim@gmail.com>
2025-09-15 09:28:39 +0300

    (chore): add db test page

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Entity\Project;
+use App\Repository\ProjectRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -41,7 +42,47 @@ final class TestController extends AbstractController
         return $this->json([
             'active_projects_count' => count($activeProjects),
             'overdue_tasks_count' => count($overdueTasks),
-            'task_statistics' => $taskStats
+            'task_statistics' => $taskStats,
+            'projectTasksSummary' => $projectRepo->getProjectTaskSummary(),
         ]);
+    }
+
+    #[Route('/test/db/', name: 'app_test_db')]
+    public function db_test(ProjectRepository $projectRepo, \App\Repository\TaskRepository $taskRepo): Response
+    {
+        $projects = $projectRepo->findAll();
+        $tasks = $taskRepo->findAll();
+        $overdueTasks = $taskRepo->findOverdueTasks();
+        $taskStats = $taskRepo->getTaskStatistics();
+
+        return $this->render('test/db_test.html.twig', [
+            'projects' => $projects,
+            'tasks' => $tasks,
+            'overdue_tasks' => $overdueTasks,
+            'task_stats' => $taskStats
+        ]);
+    }
+
+    #[Route('/test/db_create', name:'app_test_create')]
+    public function createSample(EntityManagerInterface $em): Response
+    {
+        $project = new Project();
+        $project->setName('TestProject'.date('H:i:s'));
+        $project->setDescription('This project was created through the web interface');
+
+        $task = new Task();
+        $task->setProject($project);
+        $task->setTitle('Test Task'.date('H:i:s'));
+        $task->setDescription('This task was created to test database functionality');
+        $task->setPriority(Task::PRIORITY_HIGH);
+        $task->setDueDate(new \DateTimeImmutable('+3 days'));
+
+        $em->persist($project);
+        $em->persist($task);
+        $em->flush();
+
+        $this->addFlash('success', 'Sample project and task created successfully!');
+
+        return $this->redirectToRoute('app_test_db');
     }
 }
--- src/Repository/ProjectRepository.php
+++ src/Repository/ProjectRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Project;
+use App\Entity\Task;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -44,6 +45,35 @@ class ProjectRepository extends ServiceEntityRepository
             ->leftJoin('p.tasks', 't')
             ->andWhere('p.id = :id')
             ->setParameter('id', $id)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findProjectsWithRecentTasks(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->select('p', 't')
+            ->innerJoin('p.tasks', 't')
+            ->andWhere('t.createdAt >= :weekAgo')
+            ->setParameter('weekAgo', new \DateTimeImmutable('-1 week'))
+            ->orderBy('p.name', 'ASC')
+            ->addOrderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function getProjectTaskSummary(): array
+    {
+        return $this->createQueryBuilder('p')
+            ->select([
+                'p.id',
+                'p.name',
+                'COUNT(t.id) as TotalTasks',
+                'SUM(CASE WHEN t.status = :completed THEN 1 ELSE 0 END) as completedTasks'
+            ])
+            ->leftJoin('p.tasks', 't')
+            ->setParameter('completed', Task::STATUS_COMPLETED)
+            ->groupBy('p.id')
             ->getQuery()
             ->getResult();
     }
